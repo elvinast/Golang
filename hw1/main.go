c8bd7a115acbe3cd51e71afe57e0227a951c48f6
Elvina <elvinashm@gmail.com>
2021-09-12 13:13:41 +0600

    fixed

--- hw1/main.go
+++ hw1/main.go
@@ -8,18 +8,18 @@ import (
 func Sqrt(x float64) float64 {
 	z := 1.0
 	cnt := 0
-	for i := 0; i <= int(x); i++ {
+	for {
+		cnt++
 		fmt.Println("number of iterations:", cnt, z)
-		if z == z - (z * z - x) / (2 * z) {
+		if math.Abs(z - z - (z * z - x) / (2 * z)) <= 1e-6 {
 			break
 		}
 		z -= (z * z - x) / (2 * z)
-		cnt++
 	}
 	return z
 }
 
 func main() {
-	fmt.Println(Sqrt(901))
-	fmt.Println(math.Sqrt(901))
+	fmt.Println(Sqrt(800))
+	fmt.Println(math.Sqrt(800))
 }
