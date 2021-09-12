979be5ae6c177e00f342df9cb1905f5bea993338
Elvina <elvinashm@gmail.com>
2021-09-12 13:07:05 +0600

    home work 1

--- hw1/main.go
+++ hw1/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func Sqrt(x float64) float64 {
+	z := 1.0
+	cnt := 0
+	for i := 0; i <= int(x); i++ {
+		fmt.Println("number of iterations:", cnt, z)
+		if z == z - (z * z - x) / (2 * z) {
+			break
+		}
+		z -= (z * z - x) / (2 * z)
+		cnt++
+	}
+	return z
+}
 
 func main() {
-	fmt.Println("hello")
+	fmt.Println(Sqrt(901))
+	fmt.Println(math.Sqrt(901))
 }
